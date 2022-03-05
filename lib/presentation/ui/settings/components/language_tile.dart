51e839d3f76af900e14402ef1697ad68c9b96303
Kardenvan <kmlichling123@gmail.com>
2022-03-06 00:00:54 +0300

    Поменял отображение выбора языка

--- lib/presentation/ui/settings/components/language_tile.dart
+++ lib/presentation/ui/settings/components/language_tile.dart
@@ -7,45 +7,88 @@ class LanguageTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(AppLocalizationsWrapper.of(context).app_language),
-      trailing: Container(
-        decoration: const BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black38,
-              blurRadius: 10,
-              spreadRadius: 2,
-            ),
-          ],
-        ),
-        child: BlocBuilder<AppSettingsCubit, AppSettingsState>(
-          builder: (context, state) {
-            return IconButton(
-              constraints: const BoxConstraints(
-                minWidth: 35,
+      trailing: BlocBuilder<AppSettingsCubit, AppSettingsState>(
+        builder: (context, state) {
+          return Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FlagButton(
+                icon: Assets.svg.englishFlag.svg(
+                  width: 35,
+                  fit: BoxFit.fill,
+                ),
+                onPressed: () {
+                  BlocProvider.of<AppSettingsCubit>(context).setLocale(
+                    SupportedLocales.english,
+                  );
+                },
+                isSelected: state.isEnglishLocale,
               ),
-              padding: EdgeInsets.zero,
-              onPressed: () {
-                BlocProvider.of<AppSettingsCubit>(context).switchLanguage();
-              },
-              icon: _getIconByState(state),
-            );
-          },
-        ),
+              const SizedBox(
+                width: 20,
+              ),
+              FlagButton(
+                icon: Assets.svg.russianFlag.svg(
+                  width: 35,
+                  fit: BoxFit.fill,
+                ),
+                onPressed: () {
+                  BlocProvider.of<AppSettingsCubit>(context).setLocale(
+                    SupportedLocales.russian,
+                  );
+                },
+                isSelected: state.isRussianLocale,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
+}
 
-  Widget _getIconByState(AppSettingsState state) {
-    if (state.isEnglishLocale) {
-      return Assets.svg.englishFlag.svg(
-        width: 35,
-        fit: BoxFit.fill,
-      );
-    } else {
-      return Assets.svg.russianFlag.svg(
-        width: 35,
-        fit: BoxFit.fill,
-      );
-    }
+class FlagButton extends StatelessWidget {
+  const FlagButton({
+    required this.icon,
+    required this.onPressed,
+    this.isSelected = false,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget icon;
+  final bool isSelected;
+  final void Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: isSelected
+                ? Theme.of(context).colorScheme.secondary
+                : Theme.of(context).colorScheme.primary,
+            blurRadius: 10,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: BlocBuilder<AppSettingsCubit, AppSettingsState>(
+        builder: (context, state) {
+          return IconButton(
+            constraints: const BoxConstraints(
+              minWidth: 35,
+            ),
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              if (!isSelected) {
+                onPressed();
+              }
+            },
+            icon: icon,
+          );
+        },
+      ),
+    );
   }
 }
