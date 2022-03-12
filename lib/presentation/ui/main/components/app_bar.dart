5a9948eadca7fd4271121c192f1ab10ee13e4ff7
Kardenvan <kmlichling123@gmail.com>
2022-03-12 22:53:35 +0300

    1.0.0-dev-0.1.1+6 - Убрал кнопки заполнения транзакций и удаления всех

--- lib/presentation/ui/main/components/app_bar.dart
+++ lib/presentation/ui/main/components/app_bar.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_expenses_planner/config/l10n/localization.dart';
-import 'package:my_expenses_planner/di.dart';
-import 'package:my_expenses_planner/domain/use_cases/transactions/i_transactions_case.dart';
 
 class MainScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MainScreenAppBar({Key? key}) : super(key: key);
@@ -13,20 +11,20 @@ class MainScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(AppLocalizationsWrapper.of(context).main_app_bar_title),
-      actions: [
-        IconButton(
-          onPressed: () {
-            getIt<ITransactionsCase>().deleteAll();
-          },
-          icon: const Icon(Icons.delete),
-        ),
-        IconButton(
-          onPressed: () {
-            getIt<ITransactionsCase>().fillWithMockTransactions();
-          },
-          icon: const Icon(Icons.add),
-        ),
-      ],
+      // actions: [
+      //   IconButton(
+      //     onPressed: () {
+      //       getIt<ITransactionsCase>().deleteAll();
+      //     },
+      //     icon: const Icon(Icons.delete),
+      //   ),
+      //   IconButton(
+      //     onPressed: () {
+      //       getIt<ITransactionsCase>().fillWithMockTransactions();
+      //     },
+      //     icon: const Icon(Icons.add),
+      //   ),
+      // ],
     );
   }
 }
